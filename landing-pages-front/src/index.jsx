ae66ee2bbc3caf62709cf324b2dfe23a934bb60d
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-04-11 10:51:37 -0300

    Configurando styled-components

--- landing-pages-front/src/index.jsx
+++ landing-pages-front/src/index.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 import ReactDOM from "react-dom/client";
 import Home from "./templates/Home";
+import { GlobalStyles } from "./styles/globalStyles";
+import { theme } from "./styles/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>
 );
